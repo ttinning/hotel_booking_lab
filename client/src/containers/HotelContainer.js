5f6d9487e30ea2ff33e7557bc53ccdc05605299f
Tristan Tinning <tristan_tinning@hotmail.com>
2021-06-30 14:56:25 +0100

    Revert "search function complete, search by date and name"
    This reverts commit 498d664ef547c8b1f23ec8c64abbcb2a54bfab0d.

--- client/src/containers/HotelContainer.js
+++ client/src/containers/HotelContainer.js
@@ -2,16 +2,14 @@ import {useState, useEffect} from 'react';
 import HotelService from '../services/HotelService';
 import BookingList from '../components/BookingList';
 import BookingForm from '../components/BookingForm';
-import BookingFilter from '../components/BookingFilter';
 
 const HotelContainer = () => {
 
     const [bookings, setBookings] = useState([]);
-    const [filteredBookings, setFilteredBookings] = useState([])
 
     useEffect(() => {
         HotelService.getBookings()
-        .then(bookings => (setBookings(bookings), setFilteredBookings(bookings)))
+        .then(bookings => setBookings(bookings))
     }, [])
 
     const createBooking = newBooking => {
@@ -26,27 +24,11 @@ const HotelContainer = () => {
         })
     }
 
-    const filter = (searchTerm) => {
-        const prepareSearch = searchTerm.toLowerCase()
-        const filteredBookings = bookings.filter((booking) => {
-            return booking.guestName.toLowerCase().indexOf(prepareSearch) > -1
-        })
-        setFilteredBookings(filteredBookings)
-    }
-
-    const filterDate = (searchTerm) => {
-        const filteredBookings = bookings.filter((booking) => {
-            return booking.arrivalDate === searchTerm
-        })
-        setFilteredBookings(filteredBookings)
-    }
-
     return (
         <>
             <h1>Bookings</h1>
             <BookingForm createBooking={createBooking}/>
-            <BookingFilter filter={filter} filterDate={filterDate}/>
-            <BookingList bookings={filteredBookings} deleteBooking={deleteBooking} />
+            <BookingList bookings={bookings} deleteBooking={deleteBooking} />
         </>
     )
 }
